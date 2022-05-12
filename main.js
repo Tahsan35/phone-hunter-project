4b8c4d9a338abeb8f86b5dcc7afca494881100ae
Tahsan35 <tahsanknmas14@gmail.com>
2022-05-12 16:21:56 +0600

    added result area

--- main.js
+++ main.js
@@ -1,50 +1,30 @@
-/* // document.getElementById('inputValue').value
-const searchPhone = (searchText) => {
-    const searchField = document.getElementById('Search-field');
-    const searchText = searchField.value;
-
-    searchField.value = "";
-    console.log(searchText)
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-
-    fetch(url)
-    .then(res => res.json())
-    .then(data =>displayData(data));
-};
-const displayData = (data) => {
-    console.log(data.data.brand);
-    // div = document.getElementById('inputValue').value;
-
-    // for (const phone of data) {
-    //     console.log(phone);
-    // }
-} */
-
 const searchPhone = () => {
-    const searchField = document.getElementById('Search-field');
-    const searchText = searchField.value;
+        const searchField = document.getElementById('search-field');
+        const searchText = searchField.value;
 
-    searchField.value="";
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-    fetch(url)
-    .then(res => res.json())
-    .then(info => displayPhoneSearch(info.data));
-    // .data.data
-};
-const displayPhoneSearch = (phones) => {
-    const cartContainer = document.getElementById('cart-container');
-    phones.forEach(phone => {
-        const div = document.createElement('div');
+        searchField.value = '';
+
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+        fetch (url)
+        .then(res => res.json())
+        .then(info => displaySearchResult(info.data));
+} 
+
+const displaySearchResult = (data) => {
+    const searchResult = document.getElementById('search-result');
+    data.forEach(product => {
+        const div = document.createElement('div')
         div.classList.add('col')
-        div.innerHTML= ` <div class="card h-100">
-                <img src="..." class="card-img-top" alt="...">
+        div.innerHTML =`<div class="card">
+        <img src="${product.image}" class="card-img-top" alt="..." />
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <h5 class="card-title">${product.phone_name}</h5>
+        <p class="card-text">Brand name : ${product.brand}
+        </p>
         </div>
-       </div>`
-       cartContainer.appendChild(div);
-        console.log(phone); 
-    });
-
+        </div>`
+        searchResult.appendChild(div);
+        console.log(product);
+    })
+    
 }
